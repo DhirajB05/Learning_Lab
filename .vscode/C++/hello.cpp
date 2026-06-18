69a4f2b26383bf0f0f951a77b18ff285492f6155
DhirajB05 <dhirajbhavikatti05@gmail.com>
2026-06-18 14:39:00 +0530

    c++

--- .vscode/C++/hello.cpp
+++ .vscode/C++/hello.cpp
@@ -1,9 +1,12 @@
-#include <bits/stdc++.h>   
-using namespace std;
-int main() {
-    float x=3.5;
-    double y=4.5;
-    cout<<"The value of Y is:"<<y;
+#include <bits/stdc++.h>
+using namespace std;    
+int main()
+    {
+        float arr[5];
+        cout<<"Enter 5 numbers: ";
+        cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+        arr[3]+=10;
+        cout<<"\nThe numbers are: "<<arr[3];
+        return 0;
 
-    return 0;
-}
+    }
